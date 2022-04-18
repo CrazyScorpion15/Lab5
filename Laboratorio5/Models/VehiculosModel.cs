d4c7448460444a3bb6aa2bca9737c49327617190
Edryan López <edryanandre@gmail.com>
2022-04-18 14:21:11 -0600

    Modelo y controlador vehiculos

---
+++ Laboratorio5/Models/VehiculosModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio5.Models
+{
+    public class VehiculosModel
+    {
+        public int Placas { get; set; }
+        public string Color { get; set; }
+        public string Propietario { get; set; }
+        public float Longitud { get; set; }
+        public float Latitud { get; set; }
+
+    }
+}
