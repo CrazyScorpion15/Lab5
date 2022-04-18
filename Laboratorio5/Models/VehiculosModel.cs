21c8abad26b88ce25146c79f5220b2eeca7493ae
Edryan López <edryanandre@gmail.com>
2022-04-18 14:42:32 -0600

    Vistas Vehiculos

--- Laboratorio5/Models/VehiculosModel.cs
+++ Laboratorio5/Models/VehiculosModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace Laboratorio5.Models
 {
     public class VehiculosModel
     {
+        [Required]
         public int Placas { get; set; }
+        [Required]
         public string Color { get; set; }
+        [Required]
         public string Propietario { get; set; }
+        [Required]
         public float Longitud { get; set; }
+        [Required]
         public float Latitud { get; set; }
 
     }
