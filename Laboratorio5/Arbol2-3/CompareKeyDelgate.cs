1b944fb13b76f3ea3488fc23dcd37ebab9cb7dc5
Alejandro Gil <alejandro115935@gmail.com>
2022-04-19 23:15:59 -0600

    BArbol

---
+++ Laboratorio5/Arbol2-3/CompareKeyDelgate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio5.Arbol2_3
+{
+    public class CompareKeyDelgate<K>
+    {
+        public static implicit operator CompareKeyDelegate<K>(CompareKeyDelgate<K> v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Laboratorio5/Interfaz2-3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio5
+{
+    public class Interfaz2_3
+    {
+    }
+}
